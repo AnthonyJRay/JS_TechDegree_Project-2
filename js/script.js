e3845fa5f0ca2f6edf864c1b20eef756f1b5b2d1
Anthony Eriksen <ajrayid@gmail.com>
2019-07-03 19:58:21 -0600

    Still working on Pagination functionality

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const student = document.querySelectorAll('li.student-item');
 const postsPerPage = 10;
 let currentPage = 1;
 pages = [];
+pageIndex = [];
 
 // Hide list of students
 student.forEach( i => {
@@ -9,19 +10,45 @@ student.forEach( i => {
 });
 
 // Calculate number of pages
+
+
 const pageCalc = () => {
    for ( let i = 1; i <= Math.ceil((student.length / postsPerPage)); i++) {
       pages.push(i);
+      // console.log(i);
    }
 }
+pageCalc();
 
 const startIndex = () => {
-   for ( let i = 0; i <= student.length; i++) {
-      if ( i >= 0 && i < (pages * postsPerPage)) {
-         console.log(i)
-      }
-   }
-}
+   for (i = 1; i <= student.length; i++) {
 
-pageCalc();
+   }
+   console.log(i);
+   console.log(currentPage);
+         if( i > (currentPage * 10) ) {
+            
+         }
+
+}
 startIndex();
+
+// const endIndex = () => {
+
+// }
+
+// const startIndex = () => {
+//    const pageindex = pages * postsPerPage;
+//    console.log(pageindex);
+//    for ( let i = 0; i <= student.length; i++) {
+//       if ( i >= 0 && i < pages * postsPerPage) {
+//          console.log(i);
+//          console.log(pages);
+//          // console.log(x);
+//          // student.style.display = 'block';
+//          // console.log(student);
+//       }
+//    }
+// }
+// // startIndex(pageCalc(pageIndex));
+// startIndex();
